48d87c8380133df2d46ae8fcaea26a96003ebfdc
joel-leal <joel.lasi@hotmail.com>
2023-12-08 12:36:56 -0300

    exercise factorial

--- fatorial/fatorial.js
+++ fatorial/fatorial.js
@@ -1,8 +1,6 @@
-const fatorial = (fator) => {
-  console.log(fator)
-  if(fator === 1 || fator === 0) return 1
-  const result = fator*fatorial(fator-1)
+const factorial = (factor) => {
+  if(factor === 1 || factor === 0) return 1
+  const result = factor*factorial(factor-1)
   return result
 }
 
-console.log(fatorial(0))
